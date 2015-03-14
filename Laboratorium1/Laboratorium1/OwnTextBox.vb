e50b806d175c6b3249da43ac41893f06e4ca2cb9
Michal <wojtalewicz.michal@gmail.com>
2015-03-14 17:35:56 +0100

    [ADD] Laboratorium 1
    Interfejsowanie kontrolek, własnych kontrolek.

---
+++ Laboratorium1/Laboratorium1/OwnTextBox.vb
@@ -0,0 +1,17 @@
+﻿Public Class OwnTextBox
+    Inherits System.Windows.Forms.RichTextBox
+    Public Property NaszInterface As Changes
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    Private Sub rtbText_TextChanged(sender As Object, e As EventArgs) Handles Me.TextChanged
+        NaszInterface.Zmiana()
+    End Sub
+
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        Me.ResumeLayout(False)
+    End Sub
+End Class
